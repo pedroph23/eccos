1aeee55ec33ac27156eab0ddd9189c0d2d00c307
pedroph23 <30670185+pedroph23@users.noreply.github.com>
2018-09-04 17:31:30 -0300

    Iniciando o projeto

---
+++ eccos/src/main/java/com/example/eccos/EccosApplication.java
@@ -0,0 +1,12 @@
+package com.example.eccos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccosApplication.class, args);
+	}
+}
